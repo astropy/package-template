9c0ddfb62e7f9c672ba32b1d58ab6af8b6add58a
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-11-29 00:03:34 +0100

    Minor formatting fix

--- packagename/utils/__init__.py
+++ packagename/utils/__init__.py
@@ -1,2 +1,2 @@
 # This sub-module is destined for common non-package specific utility functions
-# that will ultimately be merged into astropy.util
+# that will ultimately be merged into `astropy.utils`
