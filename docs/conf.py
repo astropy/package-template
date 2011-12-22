5b860473cadce5811302a1133a914c89dbb7b909
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-12-22 20:55:56 +0100

    Fixed import in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,15 +14,15 @@
 import os
 import sys
 
-import packagename
-
 # Load all of the global Astropy configuration
 from astropy.sphinx.conf import *
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+import packagename
 
 # -- General configuration -----------------------------------------------------
 
