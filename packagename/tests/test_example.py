b10abfc577bfb0e4411783e13b069742a578a33d
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-12-22 20:50:37 +0100

    Moved test_example.py to tests/

--- packagename/tests/test_example.py
+++ packagename/tests/test_example.py
@@ -1,5 +1,5 @@
-from .example_c import primes as primes_c
-from .example_mod import primes
+from ..example_c import primes as primes_c
+from ..example_mod import primes
 
 def test_primes_c():
     assert primes_c(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
