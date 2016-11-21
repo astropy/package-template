24ee7ebaf95f1094671aac9c7249c64623552bf8
Stuart Mumford <stuart@cadair.com>
2016-11-21 16:13:32 +0000

    Add check on astropy theme settings

--- {{ cookiecutter.package_name }}/docs/conf.py
+++ {{ cookiecutter.package_name }}/docs/conf.py
@@ -103,13 +103,6 @@ release = package.__version__
 # global configuration are listed below, commented out.
 
 
-# Please update these texts to match the name of your package.
-html_theme_options = {
-    'logotext1': 'package',  # white,  semi-bold
-    'logotext2': '-template',  # orange, light
-    'logotext3': ':docs'   # white,  light
-    }
-
 # Add any paths that contain custom themes here, relative to this directory.
 # To use a different custom theme, add the directory containing the theme.
 #html_theme_path = []
@@ -118,9 +111,18 @@ html_theme_options = {
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
 #html_theme = None
-{%- if cookiecutter.sphinx_theme != "astropy-boostrap" -%}
+{% if cookiecutter.sphinx_theme != "astropy-boostrap" %}
 html_theme = {{ cookiecutter.sphinx_theme }}
-{%- endif -%}
+
+{% else %}
+# Please update these texts to match the name of your package.
+html_theme_options = {
+    'logotext1': 'package',  # white,  semi-bold
+    'logotext2': '-template',  # orange, light
+    'logotext3': ':docs'   # white,  light
+    }
+
+{% endif %}
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
