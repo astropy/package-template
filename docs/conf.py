9667c7daa01681b4219b4f669ef1d8c997a86c3f
Stuart Mumford <stuart@cadair.com>
2017-02-28 15:52:16 +0000

    not using static atm

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ html_favicon = os.path.join(html_theme_path[0], html_theme, 'static',
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 html_sidebars = {
     '**': ['localtoc.html'],
