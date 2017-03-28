ddd74da2fcf1dcfc8c9db186413ce910b8e62984
Stuart Mumford <stuart@cadair.com>
2017-03-28 17:03:14 +0100

    Drop the no PR restriction for testing. Also add a no extra-content

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -ev
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" ]]; then
+set -eva
+# "${TRAVIS_PULL_REQUEST}" = "false" &&
+if [[ "$TRAVIS_OS_NAME" = "linux" && "EXTRA_CONTENT" = "" ]]; then
     #openssl aes-256-cbc -K $encrypted_b62220aba756_key -iv $encrypted_b62220aba756_iv -in publish-key.enc -out ~/.ssh/publish-key -d
     #chmod u=rw,og= ~/.ssh/publish-key
     #echo "Host github.com" >> ~/.ssh/config
