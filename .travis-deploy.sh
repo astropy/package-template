40e2c729b2649b4ddca9e2044d35ac3ccc39d563
Stuart Mumford <stuart@cadair.com>
2017-03-28 16:53:36 +0100

    Get rendered template pushing without history on the rendered branch
    get crypto key working
    
    Testing travis deploy.
    
    Drop the no PR restriction for testing. Also add a no extra-content
    
    Travis bug fixes
    
    I touchy the fish
    
    this was lying around
    
    try this!
    
    more debugging
    
    only on the one build please travis
    
    damn it bash
    
    damn it mumford
    
    small bug fixes and improvements
    
    Add encrypted private deploy key
    
    wtf travis
    
    Revert "wtf travis"
    
    This reverts commit 68342410ab095edc2892e0b4cf674783de763d24.
    
    try it again
    
    We don't PR
    
    more travis deploy testing
    
    manoeuvre into the correct dir
    
    directory madness
    
    fix the remote command
    
    push it!
    
    Reset from testing

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#set -eva
+set -eva
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" && -z "$EXTRA_CONTEXT" ]]; then
-    openssl aes-256-cbc -K $encrypted_c554857c6215_key -iv $encrypted_c554857c6215_iv -in github_deploy_key.enc -out github_deploy_key -d
+    openssl aes-256-cbc -K $encrypted_c554857c6215_key -iv $encrypted_c554857c6215_iv -in github_deploy_key.enc -out ~/.ssh/publish-key -d
     chmod u=rw,og= ~/.ssh/publish-key
     echo "Host github.com" >> ~/.ssh/config
     echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
+    cd ../test/packagename
     git --version
-    git remote set-url origin git@github.com:astropy/package-template
-    git fetch origin master:master
-    git checkout master
-    ls
+    git remote add origin git@github.com:astropy/package-template
+    git remote update
+    git push origin master:rendered -f
 fi
