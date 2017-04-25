be6e9e4e1364d5bc985a0664abad89afc46821e1
Stuart Mumford <stuart@cadair.com>
2017-04-25 16:46:20 +0100

    Fix rendering and keeping history
    special render travis job
    
    fix conditionals
    
    don't delete the git repo
    
    config
    
    correct branch
    
    re-order

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -eva
 
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" && -z "$EXTRA_CONTEXT" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" && $TASK='render' ]]; then
     openssl aes-256-cbc -K $encrypted_c554857c6215_key -iv $encrypted_c554857c6215_iv -in github_deploy_key.enc -out ~/.ssh/publish-key -d
     chmod u=rw,og= ~/.ssh/publish-key
     echo "Host github.com" >> ~/.ssh/config
     echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
     cd ../test/
     git clone git@github.com:astropy/package-template rendered
-    rsync -avz --delete packagename/ rendered/
     cd rendered
     git --version
+    git config --global user.name "Travis CI"
+    git config --global user.email "travis@travis.ci"
     git remote update
-    git add .
+    git checkout rendered
+    rsync -avz --delete --exclude .git/ ../packagename/ ./
+    git add -A
     git commit -m "Update rendered version to ""$TRAVIS_COMMIT"
-    git push origin master:rendered
+    git push origin rendered
 fi
