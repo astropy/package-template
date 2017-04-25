46298f412e39b1766d9061a0c459befe670df88c
Stuart Mumford <stuart@cadair.com>
2017-04-25 17:56:05 +0100

    fix task conditional

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eva
 
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" && $TASK='render' ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" && $TASK = 'render' ]]; then
     openssl aes-256-cbc -K $encrypted_c554857c6215_key -iv $encrypted_c554857c6215_iv -in github_deploy_key.enc -out ~/.ssh/publish-key -d
     chmod u=rw,og= ~/.ssh/publish-key
     echo "Host github.com" >> ~/.ssh/config
