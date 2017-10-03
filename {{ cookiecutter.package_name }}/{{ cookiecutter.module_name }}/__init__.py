ea4fb601cb1d0a5af6153b27940cd411e4d7a696
Brigitta Sipocz <b.sipocz@gmail.com>
2017-10-03 14:28:24 +0100

    Remove superflous docstring

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,9 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is a package.
-"""
-
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
