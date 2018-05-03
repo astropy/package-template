d41520f2fc259422ab7ec724879060b89e427d2b
Travis CI <travis@travis.ci>
2018-05-03 12:21:07 +0000

    Update rendered version to 81c9b4405652ef8c38ea5049a160f4e2440225c4

--- packagename/__init__.py
+++ packagename/__init__.py
@@ -20,6 +20,4 @@ if sys.version_info < tuple((int(val) for val in __minimum_python_version__.spli
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
-
     from .example_mod import *
-
