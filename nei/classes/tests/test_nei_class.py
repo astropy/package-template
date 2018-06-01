3d537697accf73594dc63553d1f5489c2305a12b
Nick Murphy <namurphy@cfa.harvard.edu>
2018-05-31 20:31:35 -0400

    Test NEI class attributes more carefully

--- nei/classes/tests/test_nei_class.py
+++ nei/classes/tests/test_nei_class.py
@@ -1,25 +1,74 @@
 import astropy.units as u
-from plasmapy.atomic import IonizationState, IonizationStates
+from plasmapy.atomic import IonizationStates, particle_symbol
 from ..nei import NEI
 import numpy as np
 import pytest
 
+inputs_dict = {'H': [0.9, 0.1], 'He': [0.5, 0.3, 0.2]}
+abundances = {'H': 1, 'He': 0.1}
+
+time_array = np.array([-3, 90]) * u.s
+T_e_array = np.array([4e4, 6e4]) * u.K
+n_H_array = np.array([1e9, 5e8]) * u.cm ** -3
+
+
 tests = {
 
     'basic': {
-        'inputs': {'H': [0.9, 0.1], 'He': [0.5, 0.3, 0.2]},
-        'abundances': {'H': 1, 'He': 0.1},
-        'T_e': np.array([1e4, 6e4]) * u.K,
-        'n_H': 1e15 * u.m ** -3,
-        'time_input': np.array([0, 1000]) * u.s,
+        'inputs': inputs_dict,
+        'abundances': abundances,
+        'T_e': T_e_array,
+        'n_H': n_H_array,
+        'time_input': time_array,
         'time_start': 0 * u.s,
         'time_max': 800 * u.s,
-        'max_steps': 10,
+        'max_steps': 5,
     },
+
+    'T_e constant': {
+        'inputs': inputs_dict,
+        'abundances': abundances,
+        'T_e': 1 * u.MK,
+        'n_H': n_H_array,
+        'time_input': time_array,
+        'time_start': 0 * u.s,
+        'time_max': 800 * u.s,
+        'max_steps': 5,
+    },
+
+    'n_e constant': {
+        'inputs': inputs_dict,
+        'abundances': abundances,
+        'T_e': T_e_array,
+        'n_H': 1e9 * u.cm ** -3,
+        'time_input': time_array,
+        'time_start': 0 * u.s,
+        'time_max': 800 * u.s,
+        'max_steps': 5,
+    },
+
+    'T_e function': {
+        'inputs': inputs_dict,
+        'abundances': abundances,
+        'T_e': lambda time: 1e4 * (1 + time/u.s) * u.K,
+        'n_H': 1e15 * u.cm **-3,
+        'time_max': 800 * u.s,
+        'max_steps': 5,
+    },
+
+    'n_H function': {
+        'inputs': inputs_dict,
+        'abundances': abundances,
+        'T_e': 6e4 * u.K,
+        'n_H': lambda time: 1e9 * (1 + time/u.s) * u.cm ** -3,
+        'time_start': 0 * u.s,
+        'time_max': 800 * u.s,
+
+    }
 
 }
 
-test_names = tests.keys()
+test_names = list(tests.keys())
 
 
 class TestNEI:
@@ -32,51 +81,71 @@ class TestNEI:
     def test_instantiate(self, test_name):
         try:
             instance = NEI(**tests[test_name])
-        except Exception:
-            raise Exception(f"Problem with test {test_name}")
-
-        self.instances[test_name] = instance
+            self.instances[test_name] = instance
+        except Exception as exc:
+            raise Exception(f"Problem with test {test_name}") from exc
 
     @pytest.mark.parametrize('test_name', test_names)
-    def test_initial(self, test_name):
+    def test_time_start(self, test_name):
+        instance = self.instances[test_name]
+        if 'time_start' in tests[test_name].keys():
+            assert tests[test_name]['time_start'] == instance.time_start
+        elif 'time_input' in tests[test_name].keys():
+            assert tests[test_name]['time_input'].min() == instance.time_start
+        else:
+            assert instance.time_start == 0 * u.s
+
+    @pytest.mark.parametrize('test_name', test_names)
+    def test_time_max(self, test_name):
+        instance = self.instances[test_name]
+        if 'time_max' in tests[test_name].keys():
+            assert tests[test_name]['time_max'] == instance.time_max
+
+    @pytest.mark.parametrize('test_name', test_names)
+    def test_initial_type(self, test_name):
         instance = self.instances[test_name]
         assert isinstance(instance.initial, IonizationStates)
-        assert isinstance(instance.abundances, dict)
-        assert isinstance(instance.T_e_input, u.Quantity)
-        assert isinstance(instance.n_H, u.Quantity)
-        assert isinstance(instance.time_input, u.Quantity)
 
-#    @pytest.mark.parametrize('test_name', test_names)
-#    def test_initialize_simulation(self, test_name):
-#        try:
-#            self.instances[test_name]._initialize_simulation()
-#        except Exception as exc:
-#            raise Exception(f"Unable to initialize simulation for test {test_name}")
+    @pytest.mark.parametrize('test_name', test_names)
+    def test_n_H_input(self, test_name):
+        actual = self.instances[test_name].n_H_input
+        expected = tests[test_name]['n_H']
+        if isinstance(expected, u.Quantity) and not expected.isscalar:
+            assert all(expected == actual)
+        else:
+            assert expected == actual
 
+    @pytest.mark.parametrize('test_name', test_names)
+    def test_T_e_input(self, test_name):
+        actual = self.instances[test_name].T_e_input
+        expected = tests[test_name]['T_e']
+        if isinstance(expected, u.Quantity) and not expected.isscalar:
+            assert all(expected == actual)
+        else:
+            assert expected == actual
 
-
-def test_nei():
-
-    try:
-        IonizationStates(['H', 'He'])
-    except Exception:
-        print('sdfasd')
-
-
-#    nei=NEI(['H', 'He'])
-
-    nei = NEI(
-        ['H', 'He'],
-        T_e=1e6*u.K,
-        n_H=1e9*u.cm**-3,
-        abundances={'H': 1, 'He': 0.1}
+    @pytest.mark.parametrize(
+        'test_name',
+        [test_name for test_name in test_names if isinstance(tests[test_name]['inputs'], dict)],
     )
+    def test_initial_ionfracs(self, test_name):
+        original_inputs = tests[test_name]['inputs']
+        original_elements = original_inputs.keys()
 
-    nei.simulate()
+        for element in original_elements:
+            assert np.allclose(
+                original_inputs[element],
+                self.instances[test_name].initial.ionic_fractions[particle_symbol(element)]
+            )
 
-    for element in nei.elements:
-        assert np.isclose(np.sum(nei.initial.ionic_fractions[element]), 1)
-        assert np.allclose(nei.abundances[element], nei.initial.abundances[element])
+    @pytest.mark.parametrize('test_name', test_names)
+    def test_simulate(self, test_name):
+        instance = self.instances[test_name]
+        try:
+            instance.simulate()
+        except Exception as exc:
+            raise ValueError("Unable to simulate.") from exc
 
-#        assert np.allclose(nei.results.ionic_fractions[element][:, 0],
-#                           nei.initial.ionic_fractions[element])
+    # Test that ionization fractions that were calculated to be in
+    # equilibrium remain in equilibrium after (1) a single long time
+    # step, and (2) a bunch of smaller time steps.
