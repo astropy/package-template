3acb9bef3d9cd28bcf4cc4608b91880ba53722f6
Brigitta Sipocz <bsipocz@gmail.com>
2019-06-08 14:21:04 -0700

    Merge pull request #409 from bsipocz/flake8_fixes
    Fixing some PEP8 for the rendered version

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -3,25 +3,29 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._{{ cookiecutter._parent_project }}_init import *
+from ._{{ cookiecutter._parent_project }}_init import *   # noqa
 # ----------------------------------------------------------------------------
 
 # Enforce Python version check during package import.
 # This is the same check as the one at the top of setup.py
 import sys
+from distutils.version import LooseVersion
 
 __minimum_python_version__ = "{{ cookiecutter.minimum_python_version }}"
+
 
 class UnsupportedPythonError(Exception):
     pass
 
-if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError("{{ cookiecutter.module_name }} does not support Python < {}".format(__minimum_python_version__))
 
-if not _ASTROPY_SETUP_:
+if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
+    raise UnsupportedPythonError("{{ cookiecutter.module_name }} does not support Python < {}"
+                                 .format(__minimum_python_version__))
+
+if not _ASTROPY_SETUP_:   # noqa
     # For egg_info test builds to pass, put package imports here.
 {%- if cookiecutter.include_example_code != 'y' %}
     pass
 {%- else %}
-    from .example_mod import *
+    from .example_mod import *   #noqa
 {%- endif %}
