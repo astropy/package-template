c6dbd6e2ea73c6f760091a090badf630b50897e6
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-29 13:41:53 +0000

    Added support for specifying required and optional dependencies

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/conftest.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/conftest.py
@@ -25,9 +25,7 @@ def pytest_configure(config):
     packagename = os.path.basename(os.path.dirname(__file__))
     TESTED_VERSIONS[packagename] = version
 
-from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
-
-# Uncomment the last line in this block to treat all DeprecationWarnings as
+# Uncomment the last two lines in this block to treat all DeprecationWarnings as
 # exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
 # as follow (although default should work for most cases).
 # To ignore some packages that produce deprecation warnings on import
@@ -37,4 +35,5 @@ from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
 # To ignore some specific deprecation warning messages for Python version
 # MAJOR.MINOR or later, add:
 #     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
+# from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
 # enable_deprecations_as_exceptions()
