fb2e354ce5a6ea76052e648768b657450d72afe1
Nathaniel Starkman <nstarkman@protonmail.com>
2020-10-16 13:48:18 -0400

    Add example
    Suggested by @bsipocz (https://github.com/astropy/package-template/pull/494#issuecomment-709568740)
    
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/extern/__init__.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/extern/__init__.py
@@ -2,4 +2,14 @@
 """
 This packages contains python packages that are bundled with the package but
 are external to it, and hence are developed in a separate source tree.
+
+Docstrings of this directory contents are NOT tested by default.
+For example:
+
+    >>> this is tested = False
+
+To turn on doctests, remove the line
+``doctest_norecursedirs={{ cookiecutter.module_name }}/extern/*``
+in ``setup.cfg``.
+
 """
