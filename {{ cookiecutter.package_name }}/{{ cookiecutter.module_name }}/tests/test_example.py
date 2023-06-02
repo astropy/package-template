5de149abd198d220153c089f2e39d67e22676760
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-06-02 14:20:01 -0400

    Update test_example.py
    to not mention deprecated function

--- {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/tests/test_example.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.module_name }}/tests/test_example.py
@@ -13,5 +13,5 @@ def test_deprecation():
     import warnings
     warnings.warn(
         "This is deprecated, but shouldn't raise an exception, unless "
-        "enable_deprecations_as_exceptions() called from conftest.py",
+        "you ask pytest to turn warnings into errors.",
         DeprecationWarning)
